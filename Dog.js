67ae49e2a9940cd53fe1d9f5f2ca37dc2681c55c
Irena Romendik <kidnemor@gmail.com>
2021-09-11 02:11:51 -0400

    update boundaries

--- Dog.js
+++ Dog.js
@@ -6,22 +6,28 @@ class Dog {
     this.name = params.name;
     this.attitude = params.attitude;
     this.size = params.size;
-    this.eyeColor = random(["magenta", "green", "magenta", "orange", "blue", "brown"]); // params.eyeColor;
+    this.eyeColor = random(["magenta", "green", "purple", "red", "yellow", "orange", "blue", "brown"]); // params.eyeColor;
     this.bark = params.bark;
-    this.place = params.place;
     this.wink = false;
-    this.stepX = 1;
-    this.stepY = 1;
+    this.acceleration = Math.floor(random(3)) + 1
+    this.stepX = this.acceleration;
+    this.stepY = this.acceleration;
+    this.boundaries = {
+      x: params.place[0],
+      y: params.place[1],
+      w: params.place[0] + this.size[0] * 2,
+      h: params.place[1] + this.size[1],
+    }
   }
 
   talk() {
     // print(`${this.name} (${this.attitude}ly): ${this.bark}`);
     // display talk text next to the dog
-    text(`${this.name} (${this.attitude}ly): ${this.bark}`, this.place[0], this.place[1]);
+    text(`${this.name} (${this.attitude}ly): ${this.bark}`, this.boundaries.x, this.boundaries.h);
   }
 
   sit() {
-    if ((mouseX - this.size[0]) >= this.place[0] && (mouseY - this.size[1]) >= this.place[1]) {
+    if (mouseX >= this.boundaries.x && mouseX <= this.boundaries.w && mouseY >= this.boundaries.y && mouseY <= this.boundaries.h) {
       this.wink = true;
       this.talk();
     } else {
@@ -30,57 +36,61 @@ class Dog {
 
     fill(color(this.eyeColor));
     noStroke();
-    ellipse(this.place[0], this.place[1], this.size[0], this.size[1]);
+    ellipse(this.boundaries.x, this.boundaries.y, this.size[0], this.size[1]);
     if (this.wink === true) fill(color('black'));
-    ellipse(this.place[0] + this.size[0], this.place[1], this.size[0], this.size[1]);
+    ellipse(this.boundaries.x + this.size[0], this.boundaries.y, this.size[0], this.size[1]);
   }
 
   wonderWalk() {
     let dice = Math.floor(random(8));
 
     // prevent dogs going over the border
-    if (this.place[0] >= width) {
+    if (this.boundaries.w >= width) {
       this.stepX = -1;
     }
-    if (this.place[0] <= 0) {
+    if (this.boundaries.x <= 0) {
       this.stepX = 1;
     }
-    if (this.place[1] >= height) {
+    if (this.boundaries.h >= height) {
       this.stepY = -1;
     }
-    if (this.place[1] <= 0) {
+    if (this.boundaries.y <= 0) {
       this.stepY = 1;
     }
 
     switch (dice) {
       case 1:
-        this.place[0] += this.stepX;
+        this.boundaries.x += this.stepX;
         break;
       case 2:
-        this.place[1] += this.stepY;
+        this.boundaries.y += this.stepY;
         break;
       case 3:
-        this.place[0] -= this.stepX;
+        this.boundaries.x -= this.stepX;
         break;
       case 4:
-        this.place[1] -= this.stepY;
+        this.boundaries.y -= this.stepY;
         break;
       case 5:
-        this.place[0] += this.stepX * 5;
+        this.boundaries.x += this.stepX * this.acceleration; // bigger step
         break;
       case 6:
-        this.place[1] += this.stepY * 5;
+        this.boundaries.y += this.stepY * this.acceleration;
         break;
       case 7:
-        this.place[0] -= this.stepX * 5;
+        this.boundaries.x -= this.stepX * this.acceleration;
         break;
       case 8:
-        this.place[1] -= this.stepY * 5;
+        this.boundaries.y -= this.stepY * this.acceleration;
         break;
 
       default:
         break;
     }
+
+    // update boundaries
+    this.boundaries.w = this.boundaries.x + this.size[0] * 2;
+    this.boundaries.h = this.boundaries.y + this.size[1];
 
     this.sit();
   }
--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ let names = ["Fanya", "Bubble", "Zak", "Buddy", "Mops", "Lala", "Rafael", "Mozar
 
 function setup() {
   createCanvas(720, 480);
-  let stepH = width / (names.length+2);
-  let stepV = height / (names.length+2);
+  let stepH = width / (names.length + 2);
+  let stepV = height / (names.length + 2);
 
   for (let n = 0; n < names.length; n++) {
     let options = {
@@ -22,7 +22,7 @@ function setup() {
       size: random(sizes),
       eyesColor: random(eyeColors),
       bark: random(barks),
-      place: [stepH * (n+1), stepV * (n+1)],
+      place: [stepH * (n + 1), stepV * (n + 1)],
     };
     dogs.push(new Dog(options));
     dogs[n].sit();
@@ -31,7 +31,13 @@ function setup() {
 
 function draw() {
   background('#fff');
-  for (let i=0; i<dogs.length; i++){
+//   for (let i = 0; i < dogs.length; i++) {
+//     dogs[i].sit();
+//   }
+// }
+
+// function keyPressed() {
+  for (let i = 0; i < dogs.length; i++) {
     dogs[i].wonderWalk();
   }
 }
