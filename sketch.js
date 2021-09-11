7a9ec4f32935ac5d5b2b85d902d0636f1234077a
Irena Romendik <kidnemor@gmail.com>
2021-09-11 00:30:07 -0400

    sitting dogs in diagonal

--- sketch.js
+++ sketch.js
@@ -1,23 +1,29 @@
 "use strict";
 let dogs = [];
-let names = ["Fanya", "Bubble", "Zak", "Buddy", "Mops", "Lala", "Rafael", "Mozart"], 
-    attitudes = ["easygoing", "friendly", "complicated", "smily", "dramatic"], 
-    sizes = [[10, 10], [20, 20], [30,30]], 
-    eyeColors = ["magenta","green", "magenta", "orange", "blue", "brown"], 
-    barks = ["Rhoof!", "Tiaf-tiaf!", "Grrrrr!", "Bark!", "Sniff-snoof!", "Oh-la-la!"];
+let names = ["Fanya", "Bubble", "Zak", "Buddy", "Mops", "Lala", "Rafael", "Mozart"],
+  attitudes = ["easygoing", "friendly", "complicated", "smily", "dramatic"],
+  sizes = [
+    [10, 10],
+    [20, 20],
+    [30, 30]
+  ],
+  eyeColors = ["magenta", "green", "magenta", "orange", "blue", "brown"],
+  barks = ["Rhoof!", "Tiaf-tiaf!", "Grrrrr!", "Bark!", "Sniff-snoof!", "Oh-la-la!"];
 
 function setup() {
   createCanvas(720, 480);
-  
-  for (let n=0; n<names.length; n++){
-    let options = { 
-        name: names[n],
-        attitude: random(attitudes),
-        size: random(sizes),
-        eyesColor: random(eyeColors),
-        bark: random(barks),
-        place: [width/(names.length+1)*(n+1), height/2],
-      };
+  let stepH = width / (names.length+2);
+  let stepV = height / (names.length+2);
+
+  for (let n = 0; n < names.length; n++) {
+    let options = {
+      name: names[n],
+      attitude: random(attitudes),
+      size: random(sizes),
+      eyesColor: random(eyeColors),
+      bark: random(barks),
+      place: [stepH * (n+1), stepV * (n+1)],
+    };
     dogs.push(new Dog(options));
     dogs[n].sit();
   }
